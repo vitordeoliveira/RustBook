8351daaf4960eb854b1ef3397d5d0a5177ea0d3f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-08-24 10:37:59 +0200

    Cap 1

---
+++ Cap_1/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d
@@ -0,0 +1,5 @@
+/Users/WWYUZ47/Dev/Study/Rustlang/RustBook/Cap_1/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9: src/main.rs
+
+/Users/WWYUZ47/Dev/Study/Rustlang/RustBook/Cap_1/hello_cargo/target/debug/deps/hello_cargo-dc2c1cfcfb3e95b9.d: src/main.rs
+
+src/main.rs:
---
+++ Cap_1/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/WWYUZ47/Dev/Study/Rustlang/RustBook/Cap_1/hello_cargo/target/release/hello_cargo: /Users/WWYUZ47/Dev/Study/Rustlang/RustBook/Cap_1/hello_cargo/src/main.rs
