ddccd05f919fb3a18a34e078be958735877c57a0
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-09-11 08:13:13 +0200

    chapter 5 init

---
+++ cap_5/exemples/src/main.rs
@@ -0,0 +1,57 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    // Note that the entire instance must be mutable; Rust doesn’t allow us to mark only certain fields as mutable.
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    // struct update syntax
+
+    // without
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+    // with
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    // tuple structs
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // unit-like structs
+    struct AlwaysEqual;
+    let subject = AlwaysEqual;
+
+    // 5.2
+    // An Example Program Using Structs
+
+}
