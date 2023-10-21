3d2b5dc8d5efb154a7912b3a76b6d4dcaf680ee5
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-10-21 11:52:19 +0200

    lifetimes
    Explains why and when U will need to care about lifetime anotations

--- chapter_10/README.md
+++ chapter_10/README.md
@@ -2,7 +2,8 @@
 
 Every programming language has tools for effectively handling the duplication of concepts. In Rust, one such tool is generics: abstract stand-ins for concrete types or other properties. We can express the behavior of generics or how they relate to other generics without knowing what will be in their place when compiling and running the code.
 
-Functions can take parameters of some generic type, instead of a concrete type like i32 or String, in the same way a function takes parameters with unknown values to run the same code on multiple concrete values. In fact, we’ve already used generics in Chapter 6 with Option<T>, Chapter 8 with Vec<T> and HashMap<K, V>, and Chapter 9 with Result<T, E>. In this chapter, you’ll explore how to define your own types, functions, and methods with generics!
+Functions can take parameters of some generic type, instead of a concrete type like i32 or String, in the same way a function takes parameters with unknown values to run the same code on multiple concrete values.
+In fact, we’ve already used generics in Chapter 6 with Option<T>, Chapter 8 with Vec<T> and HashMap<K, V>, and Chapter 9 with Result<T, E>. In this chapter, you’ll explore how to define your own types, functions, and methods with generics!
 
 First, we’ll review how to extract a function to reduce code duplication. We’ll then use the same technique to make a generic function from two functions that differ only in the types of their parameters. We’ll also explain how to use generic types in struct and enum definitions.
 
@@ -15,3 +16,23 @@ Finally, we’ll discuss lifetimes: a variety of generics that give the compiler
 A trait defines functionality a particular type has and can share with other types. We can use traits to define shared behavior in an abstract way. We can use trait bounds to specify that a generic type can be any type that has certain behavior.
 
 Note: Traits are similar to a feature often called interfaces in other languages, although with some differences.
+
+## Validating References with Lifetimes
+
+Lifetimes are another kind of generic that we’ve already been using. Rather than ensuring that a type has the behavior we want, lifetimes ensure that references are valid as long as we need them to be.
+
+"Permissions Are Returned At The End of a Reference's Lifetime" is that every reference in Rust has a lifetime, which is the scope for which that reference is valid.
+
+Annotating lifetimes is not even a concept most other programming languages have, so this is going to feel unfamiliar.
+Although we won’t cover lifetimes in their entirety in this chapter, we’ll discuss common ways you might encounter lifetime syntax so you can get comfortable with the concept.
+
+Lifetime Elision
+The compiler has 3 rules for apply the lifetimes in Functions
+1 for the input, and 2 for the output
+Lifetimes on function or method parameters are called input lifetimes, and lifetimes on return values are called output lifetimes.
+
+     1 - The first rule is that the compiler assigns a different lifetime parameter to each lifetime in each input type
+     2 - If there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) -> &'a i32.
+     3 - f there are multiple input lifetime parameters, but one of them is &self or
+     &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters.
+     see more in https://rust-book.cs.brown.edu/ch10-03-lifetime-syntax.html#lifetime-elision
---
+++ chapter_10/lifetimes/src/main.rs
@@ -0,0 +1,105 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+/// You can receive data from diff lifetimes, but just return de lifetime you want
+fn _longest_two_lifetimes<'a, 'b>(x: &'a str, y: &'b str) -> &'b str {
+    if x.len() > y.len() {
+        "na"
+    } else {
+        y
+    }
+}
+
+// if we changed the implementation of the longest function to always return the first parameter rather than the longest string slice,
+// we wouldn’t need to specify a lifetime on the y parameter
+fn _longest_3<'a>(x: &'a str, _y: &str) -> &'a str {
+    x
+}
+// The problem is that result goes out of scope and gets cleaned up at the end of the longest function.
+// We’re also trying to return a reference to result from the function.
+// There is no way we can specify lifetime parameters that would change the dangling reference, and Rust won’t let us create a dangling reference.
+
+// fn _longest<'a>(x: &str, y: &str) -> &'a str {
+//     let result = String::from("really long string");
+//     result.as_str()
+// }
+
+// fn longest_inv(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+fn main() {
+    // Here, x has the lifetime 'b, which in this case is larger than 'a.
+    // This means r can reference x because Rust knows that the reference in r will always be valid while x is valid.
+    let x = 5; // ----------+-- 'b
+               //           |
+    let r = &x; // --+-- 'a  |
+                //   |       |
+    println!("r: {}", r); //   |       |
+                          // --+       |
+                          // ----------+
+
+    // Generic Lifetimes in Functions
+    // Lifetime Annotation Syntax
+    // &i32        // a reference
+    // &'a i32     // a reference with an explicit lifetime
+    // &'a mut i32 // a mutable reference with an explicit lifetime
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    // Next exemple
+    let string3 = String::from("long string is long");
+    {
+        let string4 = String::from("xyz");
+        let result = longest(string3.as_str(), string4.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    // Next exemple
+    let string5 = String::from("long string is long");
+    let result2;
+    {
+        let string2 = String::from("xyz");
+        result2 = longest(string5.as_str(), string2.as_str());
+    }
+    // println!("The longest string is {}", result2);
+
+    // Lifetime Annotations in Struct Definitions
+    //
+    // A struct that holds a reference, requiring a lifetime annotation
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    // Lifetime Elision
+    // The compiler has 3 rules for apply the lifetimes in Functions
+    // 1 for the input, and 2 for the output
+    // Lifetimes on function or method parameters are called input lifetimes, and lifetimes on return values are called output lifetimes.
+    //
+    // 1 - The first rule is that the compiler assigns a different lifetime parameter to each lifetime in each input type
+    // 2 - If there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) -> &'a i32.
+    // 3 - f there are multiple input lifetime parameters, but one of them is &self or
+    // &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters.
+    // see more in https://rust-book.cs.brown.edu/ch10-03-lifetime-syntax.html#lifetime-elision
+
+    // Lifetime Annotations in Method Definitions
+}
