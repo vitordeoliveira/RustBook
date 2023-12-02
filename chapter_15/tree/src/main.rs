c6bfd2adde8428126a4acd650047f2e11a8a6a02
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-12-02 12:02:32 +0100

    Exemple tree with Weak<t> to fix the cyclebug

---
+++ chapter_15/tree/src/main.rs
@@ -0,0 +1,58 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+// A parent node should own its children: if a parent node is dropped, its child nodes should be
+// dropped as well. However, a child should not own its parent: if we drop a child node, the parent
+// should still exist. This is a case for weak references!
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        // The lack of infinite output indicates that this code didn’t create a reference cycle
+        // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        // println!("branch parent = {:?}", branch.parent.borrow().upgrade());
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
