9347f7fddec26d0fefa82d8ecde920da7beb9d4b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-01-27 10:21:35 +0100

    basic solution before refactor

--- chapter_20/hello/src/main.rs
+++ chapter_20/hello/src/main.rs
@@ -32,5 +32,12 @@ fn handle_connection(mut stream: TcpStream) {
         let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
         stream.write_all(response.as_bytes()).unwrap();
     } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
